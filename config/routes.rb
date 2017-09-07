76a9e6176c7e51a15a6507250486dd62fe22f502
Matthew <madd1778@yahoo.com>
2017-09-07 11:17:47 -0700

    added RESTful routes and Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :books                   #controller name and RESTful routes
+  root 'books#index'
 end
